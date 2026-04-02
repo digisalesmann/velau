e19f886d28f3f699b795a76a9a896303771ce061
digisalesmann <buildwithvictorhq@gmail.com>
2026-04-02 22:07:23 +0100

    fix: import notification module correctly (was notifications)

--- core/strategy_engine.py
+++ core/strategy_engine.py
@@ -13,7 +13,7 @@ from ta.volatility import AverageTrueRange
 from news.news_pipeline            import get_news_and_sentiment
 from brokers.deriv_trading_service import DerivTradingService
 import database as db
-import notifications as notif
+import notification as notif
 
 logger = logging.getLogger("XAUStrategy")
 
